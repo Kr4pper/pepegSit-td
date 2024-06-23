9515830858c53973e1311b560c406eb5cd8ce149
Kr4pper <Kr4pper@github.com>
2024-06-23 14:26:46 +0200

    working sprite movement

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import helmet from 'helmet';
+import path from 'path';
+
+const app = express();
+app.use(helmet());
+
+app.use(express.static(path.join(__dirname, '/web')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/web/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('The application is listening on port 3000!');
+});
